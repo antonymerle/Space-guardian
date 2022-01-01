cfa0cb97892ab0be594abecfe1a05aec6a9e4859
Antony Merle <antony.merle@yahoo.fr>
2022-01-01 12:06:50 +0100

    checked lower bound for enum

--- input.c
+++ input.c
@@ -4,7 +4,8 @@ void doKeyDown(SDL_KeyboardEvent* event)
 {
 	/* ignore keyboard repeat events; these could queue up and cause some unexpected things to happen,
 	and so we only want to deal with events where the key has been pushed down for the first time.*/
-	if (event->repeat == 0 && event->keysym.scancode < MAX_KEYBOARD_KEYS)
+	if (event->keysym.scancode >= MAX_KEYBOARD_KEYS || event->keysym.scancode <= SDL_SCANCODE_UNKNOWN) return;
+	if (event->repeat == 0)
 	{
 		app.keyboard[event->keysym.scancode] = 1;
 	}
@@ -14,7 +15,8 @@ void doKeyUp(SDL_KeyboardEvent* event)
 {
 	/* ignore keyboard repeat events; these could queue up and cause some unexpected things to happen,
 	and so we only want to deal with events where the key has been pushed down for the first time.*/
-	if (event->repeat == 0 && event->keysym.scancode < MAX_KEYBOARD_KEYS)
+	if (event->keysym.scancode >= MAX_KEYBOARD_KEYS || event->keysym.scancode <= SDL_SCANCODE_UNKNOWN) return;
+	if (event->repeat == 0)
 	{
 		app.keyboard[event->keysym.scancode] = 0;
 	}
