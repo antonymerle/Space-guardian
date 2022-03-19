1f435440a055e25ba738df16ba0d0025f0e4851e
Antony Merle <antony.merle@gmail.com>
2022-03-19 22:43:49 +0100

    animated title

--- title.c
+++ title.c
@@ -9,6 +9,8 @@ static SDL_Texture* titleTexture;
 static int revealH = 200;
 static int revealW = 200;
 static int timeout;
+static uint8_t animationCounter;
+static size_t spriteTitleIndex;
 
 void initTitle(void)
 {
@@ -17,9 +19,9 @@ void initTitle(void)
 
 	memset(app.keyboard, 0, sizeof(int) * MAX_KEYBOARD_KEYS);
 
-	titleTexture = loadTexture("gfx/title.png");
+	titleTexture = loadTexture("gfx/title2.png");
 	
-	timeout = FPS * 15;
+	timeout = FPS * 60;
 }
 
 static void logic(void)
@@ -62,15 +64,30 @@ static void draw(void)
 
 static void drawTitle(void)
 {
-	SDL_Rect r;
+	//SDL_Rect r;
 
-	r.x = 0;
-	r.y = 0;
+	SDL_Rect srcRect = { spriteTitleIndex * SPRITE_TITLE_WIDTH, 0, SPRITE_TITLE_WIDTH, SPRITE_TITLE_HEIGHT };
 
-	SDL_QueryTexture(titleTexture, NULL, NULL, &r.w, &r.h);
 
-	r.w = MIN(revealW, r.w);
-	r.h = MIN(revealH, r.h);
+	//r.x = 0;
+	//r.y = 0;
 
-	blitRect(titleTexture, &r, (SCREEN_WIDTH / 2) - (r.w / 2), 100);
+	//SDL_QueryTexture(titleTexture, NULL, NULL, &r.w, &r.h);
+
+	srcRect.w = MIN(revealW, srcRect.w);
+	srcRect.h = MIN(revealH, srcRect.h);
+
+	blitRect(titleTexture, &srcRect, (SCREEN_WIDTH / 2) - (SPRITE_TITLE_WIDTH / 2), 100);
+
+	animationCounter++;
+
+
+	if (animationCounter % 128 == 0 && revealW > SCREEN_WIDTH - 8)
+	{
+		spriteTitleIndex++;
+		if (spriteTitleIndex > 7)
+		{
+			spriteTitleIndex = 7;
+		}
+	}
 }
